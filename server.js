cdd36e50840d2c595629312e465400409cec794e
Kos-Mos <ChuyM203@gmail.com>
2020-10-11 17:59:47 -0400

    added middleware in server file/added comment and user model/created user-route

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'A Future Is Not Given To You.  It Is Something You Must Take For Yourself.',
+  cookie: {
+    maxAge: (1000 * 60) * 60
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server Now Listening On ${PORT}.`));
+})
