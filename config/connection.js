849d0c32ffab26e8d6c176d595f55a8202fb646c
Kos-Mos <ChuyM203@gmail.com>
2020-10-20 23:00:59 -0400

    added dialect

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,10 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(
       process.env.JAWSDB_URL,
       process.env.DB_USER,
-      process.env.DB_PW
+      process.env.DB_PW,
+      {
+        dialect: 'mysql',
+      }
     )
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
