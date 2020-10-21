e85867d3f49c6ca77cc66089c0d84c916a89b6a5
Kos-Mos <ChuyM203@gmail.com>
2020-10-20 21:27:09 -0400

    added edit handlers, tried to fix save button not working

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -5,11 +5,12 @@ async function editFormHandler(event) {
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
-  const postDescription = document
+
+  const post_description = document
     .querySelector('#post-description')
     .value.trim();
 
-  const response = await fetch(`/api/posts/${id}`, {
+  const response = await fetch('/api/posts/${id}', {
     method: 'PUT',
     body: JSON.stringify({
       title,
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'AMEEEEELLLLIAAA WATSON!!!',
+  secret: 'Watch Amelia on HOLOLIVE!!!!',
   cookie: {
     maxAge: 1000 * 60 * 60,
   },
