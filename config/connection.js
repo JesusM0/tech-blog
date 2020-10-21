ba2550cd663bb853c31ba236dc457460456bee16
Kos-Mos <ChuyM203@gmail.com>
2020-10-20 22:58:22 -0400

    modify connection

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 console.log(process.env.JAWSDB_URL);
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(
+      process.env.JAWSDB_URL,
+      process.env.DB_USER,
+      process.env.DB_PW
+    )
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
